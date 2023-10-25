4f4fcaef87dc91272bd09b547ef8c89b9fe0e7a4
HumayeraShayeraHuri <humayera.shayera.huri@g.bracu.ac.bd>
2023-10-24 21:02:36 -0700

    project initialized

---
+++ src/main/java/com/cse471/booksAndMangaStore/BooksAndMangaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cse471.booksAndMangaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAndMangaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAndMangaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cse471/booksAndMangaStore/BooksAndMangaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cse471.booksAndMangaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAndMangaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
